5a20753c48230145e0b42ed34d2a7948f5babf97
KCrowe03 <165347733+KCrowe03@users.noreply.github.com>
2024-07-28 05:58:04 -0400

    added homework

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,14 @@
-# add your code here
+
+# Kristen Crowe 
+# 7/28/24
+
+for x in range(1, 101):
+    if x % 3 == 0 and x % 5 == 0:
+        print("FizzBuzz")
+    elif x % 3 == 0:
+        print("Fizz")
+    elif x % 5 == 0:
+        print("Buzz")
+    else:
+        print(x)
 
